52d7bf07da6b3a1f594fa68c4b0d727694f1032e
NepoGostu <andruavanshuls@gmail.com>
2023-03-21 17:42:03 +0300

    -change rout

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './css/general.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 const appElement = document.getElementById('app')
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   appElement,
 )
